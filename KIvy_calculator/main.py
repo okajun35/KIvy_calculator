ca5333b03019bdb46b0aa153afb118d1b363b80b
okajun35 <hddwm390@yahoo.co.jp>
2016-09-06 21:06:38 +0900

    説明の追加

--- KIvy_calculator/main.py
+++ KIvy_calculator/main.py
@@ -66,6 +66,8 @@ class Calculator(App):
         text = "{}{}".format(self.root.display.text, number) # 今までの入力された文字列と入力された値が表示される
         self.root.display.text = text
 
+        print("数字「{0}」が押されました".format(number))
+
     def print_operator(self, operator):
         if self.clear_bool:
             self.clear_bool = False
@@ -73,20 +75,27 @@ class Calculator(App):
         text = "{} {} ".format(self.root.display.text, operator)
         self.root.display.text = text
 
+        print("演算子「{0}」が押されました".format(operator))
+
     def clear_display(self):
         self.root.display.text = ""
         self.clear_bool = False
 
+        print("「c」が押されました")
     def del_char(self):
         ''' "<x"を押された場合の計算結果を表示  '''
 
         self.root.display.text = self.root.display.text[:-1]
+
+        print("「<x」が押されました")
 
     def calculate(self):
         ''' "="を押された場合の計算結果を表示  '''
         try:
             self.root.display.text = str(eval(self.root.display.text)) # 単一の式を評価  例：eval("5 + 10")　は15になる
             self.clear_bool = True
+
+            print('計算完了')
         except:
             # 数字を入力せずに　'=’を押した場合などのエラー対策
             print('error　入力ミス')
